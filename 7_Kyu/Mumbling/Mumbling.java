1d27174c5d21ac22e261a6895cea35d07a06bd82
Knight-Byte <abunachar1236@gmail.com>
2020-07-23 11:24:51 +0530

    Mumbling

---
+++ 7_Kyu/Mumbling/Mumbling.java
@@ -0,0 +1,15 @@
+public class Accumul {
+    public static String accum(String s) {
+        StringBuilder bldr = new StringBuilder();
+        int i = 0;
+        for (char c : s.toCharArray()) {
+            if (i > 0)
+                bldr.append('-');
+            bldr.append(Character.toUpperCase(c));
+            for (int j = 0; j < i; j++)
+                bldr.append(Character.toLowerCase(c));
+            i++;
+        }
+        return bldr.toString();
+    }
+}
