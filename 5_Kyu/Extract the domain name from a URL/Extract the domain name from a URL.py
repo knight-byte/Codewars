a126c55e60e945d6d3a0bd876d56d243215a95d1
Knight-byte <abunachar1236@gmail.com>
2020-10-15 14:00:03 +0530

    Extract the domain name from a URL

---
+++ 5_Kyu/Extract the domain name from a URL/Extract the domain name from a URL.py
@@ -0,0 +1,8 @@
+import re
+def domain_name(url):
+    print(url)
+    url = url.replace('www', '')
+    url = url.replace('https', '')
+    url = url.replace('http', '')
+    s = re.findall("[\s]?([']?[A-Za-z0-9]+'?[A-Za-z0-9'-]*)[\s,:]?", url)
+    return s[0]
